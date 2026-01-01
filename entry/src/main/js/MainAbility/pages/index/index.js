0c968c4e264a7dc7a24e79e81f6a66733d7b4a63
Selçuk Arda Özcan <selcukardaozcan@hotmail.com>
2026-01-02 00:54:15 +0300

    anasayfada ilk omnitrix_base.png var ve üsütünde uzaylı yok , sağ sol yaptıkça uzaylı geliyor

--- entry/src/main/js/MainAbility/pages/index/index.js
+++ entry/src/main/js/MainAbility/pages/index/index.js
@@ -1,17 +1,22 @@
 export default {
     data: {
+        // Durum Değişkenleri
         currentIndex: 0,
         isTransformed: false,
         isRecharging: false,
         isAnimating: false,
+
+        // YENİ: Başlangıçta Omnitrix 'Baz' modunda (Uzaylı yok)
+        isBaseMode: true,
+
         imageScale: 1.0,
 
         // Görseller
-        currentImageSrc: "",
+        currentImageSrc: "", // Başlangıçta boş
         nextImageSrc: "",
 
-        // Opaklık değerleri (0 ile 1 arası)
-        currentOpacity: 1,
+        // YENİ: Başlangıçta mevcut uzaylı görünmez (0)
+        currentOpacity: 0,
         nextOpacity: 0,
 
         alienList: [
@@ -21,20 +26,30 @@ export default {
             "/common/images/diamondhead.png"
         ],
 
-        // Animasyon sayacı
+        // Animasyon zamanlayıcıları
         animationStep: 0,
         animationInterval: null
     },
 
     onInit() {
+        // Uygulama açıldığında ilk uzaylıyı belleğe al ama GÖSTERME (Opacity 0)
         this.currentImageSrc = this.alienList[this.currentIndex];
+        this.currentOpacity = 0; // Kilit nokta burası: Görünmez başlıyor
+        this.isBaseMode = true;
     },
 
     handleSwipe(e) {
         if (this.isTransformed || this.isRecharging || this.isAnimating) return;
 
+        // EĞER BAZ MODUNDAYSA (İLK AÇILIŞ)
+        if (this.isBaseMode) {
+            // Sağa da çeksen sola da çeksen ilk uzaylıyla açılsın
+            this.runBaseToAlienAnimation();
+            return;
+        }
+
+        // NORMAL MOD (UZAYLILAR ARASI GEÇİŞ)
         let nextIndex = this.currentIndex;
-        // Yön algılama
         if (e.direction === 'left' || e.direction === 2) {
             nextIndex = (this.currentIndex + 1) % this.alienList.length;
         } else if (e.direction === 'right' || e.direction === 1) {
@@ -43,37 +58,67 @@ export default {
             return;
         }
 
-        this.runManualAnimation(nextIndex);
+        this.runAlienToAlienAnimation(nextIndex);
     },
 
-    runManualAnimation(nextIndex) {
+    // YENİ: Hiçlikten Uzaylıya Geçiş Animasyonu
+    runBaseToAlienAnimation() {
         this.isAnimating = true;
-        this.nextImageSrc = this.alienList[nextIndex];
+        this.nextImageSrc = this.alienList[this.currentIndex]; // İlk uzaylıyı hazırla
 
-        // Başlangıç değerleri
-        this.currentOpacity = 1;
+        this.currentOpacity = 0; // Mevcut (Boş) zaten görünmez
         this.nextOpacity = 0;
         this.animationStep = 0;
 
-        // Her 30ms'de bir çalışan zamanlayıcı kuruyoruz (FPS mantığı)
-        // Toplam 10 adımda geçiş yapacak (30ms * 10 = 300ms sürer)
         this.animationInterval = setInterval(() => {
             this.animationStep++;
 
-            // Opaklığı 0.1 artır/azalt
-            this.nextOpacity = (this.animationStep / 10);      // 0.1, 0.2, ... 1.0
-            this.currentOpacity = 1 - (this.animationStep / 10); // 0.9, 0.8, ... 0.0
+            // Sadece gelen uzaylıyı parlat (Fade In)
+            this.nextOpacity = (this.animationStep / 10);
 
-            // Animasyon bitti mi?
             if (this.animationStep >= 10) {
-                clearInterval(this.animationInterval); // Zamanlayıcıyı durdur
-                this.finalizeTransition(nextIndex);
+                clearInterval(this.animationInterval);
+                this.finalizeBaseTransition();
             }
         }, 30);
     },
 
-    finalizeTransition(nextIndex) {
-        // Değerleri temizle ve sabitle
+    finalizeBaseTransition() {
+        // Artık Baz modundan çıktık, uzaylı modundayız
+        this.isBaseMode = false;
+
+        // Gelen uzaylıyı ana uzaylı yap
+        this.currentImageSrc = this.nextImageSrc;
+        this.currentOpacity = 1;
+        this.nextOpacity = 0;
+
+        this.isAnimating = false;
+    },
+
+    // Mevcut: Uzaylıdan Uzaylıya Geçiş (Cross-Fade)
+    runAlienToAlienAnimation(nextIndex) {
+        this.isAnimating = true;
+        this.nextImageSrc = this.alienList[nextIndex];
+
+        this.currentOpacity = 1;
+        this.nextOpacity = 0;
+        this.animationStep = 0;
+
+        this.animationInterval = setInterval(() => {
+            this.animationStep++;
+
+            // Biri sönerken diğeri yanar
+            this.nextOpacity = (this.animationStep / 10);
+            this.currentOpacity = 1 - (this.animationStep / 10);
+
+            if (this.animationStep >= 10) {
+                clearInterval(this.animationInterval);
+                this.finalizeAlienTransition(nextIndex);
+            }
+        }, 30);
+    },
+
+    finalizeAlienTransition(nextIndex) {
         this.currentIndex = nextIndex;
         this.currentImageSrc = this.alienList[nextIndex];
 
@@ -84,7 +129,8 @@ export default {
     },
 
     transform() {
-        if (this.isTransformed || this.isRecharging || this.isAnimating) return;
+        // Baz modundaysan (uzaylı yoksa) dönüşemezsin!
+        if (this.isBaseMode || this.isTransformed || this.isRecharging || this.isAnimating) return;
 
         this.isTransformed = true;
         this.imageScale = 1.3;
