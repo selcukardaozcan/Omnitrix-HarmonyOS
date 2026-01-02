4546358d31205cc060c907c5ad2270bd593d9609
Selçuk Arda Özcan <selcukardaozcan@hotmail.com>
2026-01-02 09:10:04 +0300

    sarj saniyesi 6

--- entry/src/main/js/MainAbility/pages/index/index.js
+++ entry/src/main/js/MainAbility/pages/index/index.js
@@ -169,6 +169,6 @@ export default {
         setTimeout(() => {
             this.resetToInitialState();
             console.info("Sistem Resetlendi.");
-        }, 5000);
+        }, 6000);
     }
 }
